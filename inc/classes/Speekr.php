a7b7faf5fc306c25e5f2a9493bdb115589e4ea53
Geoffrey Crofte <support@creativejuiz.com>
2017-10-08 20:51:20 +0200

    Introduce Speekr() main class

---
+++ inc/classes/Speekr.php
@@ -0,0 +1,102 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Cheatin\' uh?' );
+}
+
+class Speekr {
+
+	public function __construct() {
+		add_action( 'init', array( $this, 'load_textdomain' ) );
+		
+		$this->is_network();
+
+		$this->includes();
+
+		if ( is_admin() ) {
+			$this->includes_admin();
+		} else {
+			$this->includes_front();
+		}
+	}
+
+
+	/**
+	 * Set a define for network activation.
+	 *
+	 * @return void
+	 * @since  1.0
+	 * @author Geoffrey Crofte
+	 */
+	public function is_network() {
+		$is_nw_activated = function_exists( 'is_plugin_active_for_network' ) && is_plugin_active_for_network( SPEEKR_SLUG . '/' . SPEEKR_SLUG . '.php' ) ? true : false;
+
+		define( 'SPEEKR_NETWORK_ACTIVATED', $is_nw_activated );
+	}
+
+	/**
+	 * Make plugin translatable
+	 *
+	 * @return void
+	 * @since  1.0
+	 * @author Geoffrey Crofte
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'speekr', false, SPEEKR_DIRBASENAME . '/languages' );
+	}
+
+
+	/**
+	 * Includes front and admin components.
+	 *
+	 * @return void
+	 * @since  1.0
+	 * @author Geoffrey Crofte
+	 */
+	public function includes() {
+		add_action( 'speekr_before_includes' );
+
+		require_once( SPEEKR_DIRNAME . '/inc/functions/debug.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/common/custom-posts.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/functions/options.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/functions/settings.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/common/default-types.php' );
+
+		add_action( 'speekr_after_includes' );
+	}
+
+	/**
+	 * Includes admin components.
+	 *
+	 * @return void
+	 * @since  1.0
+	 * @author Geoffrey Crofte
+	 */
+	public function includes_admin() {
+		add_action( 'speekr_before_includes_admin' );
+
+		require_once( SPEEKR_DIRNAME . '/inc/admin/enqueues.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/admin/settings.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/admin/menus.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/admin/custom-meta-boxes.php' );
+
+		add_action( 'speekr_after_includes_admin' );
+	}
+
+	/**
+	 * Includes front components.
+	 *
+	 * @return void
+	 * @since  1.0
+	 * @author Geoffrey Crofte
+	 */
+	public function includes_front() {
+		add_action( 'speekr_before_includes_front' );
+
+		require_once( SPEEKR_DIRNAME . '/inc/front/enqueues.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/functions/markup.php' );
+		require_once( SPEEKR_DIRNAME . '/inc/front/lists.php' );
+
+		add_action( 'speekr_after_includes_front' );
+	}
+
+}
--- speekr.php
+++ speekr.php
@@ -18,49 +18,12 @@ define( 'SPEEKR_PLUGIN_NAME',	 'Speekr' );
 define( 'SPEEKR_VERSION',      '1.0' );
 define( 'SPEEKR_FILE',		 __FILE__ );
 define( 'SPEEKR_DIRNAME',		 dirname( SPEEKR_FILE ) );
+define( 'SPEEKR_CLASSES_DIR',	 SPEEKR_DIRNAME . '/inc/classes/' );
 define( 'SPEEKR_DIRBASENAME',	 basename( dirname( SPEEKR_FILE ) ) );
 define( 'SPEEKR_PLUGIN_URL',	 plugin_dir_url( SPEEKR_FILE ));
 define( 'SPEEKR_SLUG',		 'speekr' );
 define( 'SPEEKR_SETTING_SLUG', 'speekr_settings' );
 
-// Checking network activation
-$is_nw_activated = function_exists( 'is_plugin_active_for_network' ) && is_plugin_active_for_network( SPEEKR_SLUG . '/' . SPEEKR_SLUG . '.php' ) ? true : false;
+require_once( SPEEKR_CLASSES_DIR . 'Speekr.php' );
 
-define( 'SPEEKR_NETWORK_ACTIVATED', $is_nw_activated );
-
-/**
- * Make plugin translatable
- *
- * @return void
- * @since  1.0
- * @author Geoffrey Crofte
- */
-function speekr_textdomain() {
-	load_plugin_textdomain( 'speekr', false, SPEEKR_DIRBASENAME . '/languages' );
-}
-add_action( 'init', 'speekr_textdomain' );
-
-/**
- * Includes all the thing
- */
-
-// Admin and Front cases
-require_once( SPEEKR_DIRNAME . '/inc/functions/debug.php' );
-require_once( SPEEKR_DIRNAME . '/inc/common/custom-posts.php' );
-require_once( SPEEKR_DIRNAME . '/inc/functions/options.php' );
-require_once( SPEEKR_DIRNAME . '/inc/functions/settings.php' );
-require_once( SPEEKR_DIRNAME . '/inc/common/default-types.php' );
-
-// Only Admin case.
-if ( is_admin() ) {
-	require_once( SPEEKR_DIRNAME . '/inc/admin/enqueues.php' );
-	require_once( SPEEKR_DIRNAME . '/inc/admin/settings.php' );
-	require_once( SPEEKR_DIRNAME . '/inc/admin/menus.php' );
-	require_once( SPEEKR_DIRNAME . '/inc/admin/custom-meta-boxes.php' );
-}
-// Only Front case
-else {
-	require_once( SPEEKR_DIRNAME . '/inc/front/enqueues.php' );
-	require_once( SPEEKR_DIRNAME . '/inc/functions/markup.php' );
-	require_once( SPEEKR_DIRNAME . '/inc/front/lists.php' );
-}
+$speekr = new Speekr();
