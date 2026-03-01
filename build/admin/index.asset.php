f9d34b91e8d91ffb02d0c31c27e5ee06f9dbfa58
CRG <crg@foyer.lu>
2026-03-01 01:17:48 +0100

    chore(01-03): install dependencies and run production build
    - Remove stale package-lock.json (locked old versions: @wordpress/scripts@^12.1.0, node-sass, gulp)
    - Fresh npm install with updated package.json (@wordpress/scripts@^31.5.0)
    - npm run build exits 0: produces build/admin/style-index.{css,js} and build/frontend/style-style.css
    - Note: @wordpress/scripts CSS output uses style- prefix convention (style-index.css not index.css)

---
+++ build/admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '73119ec56ef87b8a71ac');
---
+++ build/frontend/style.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6ee7c325cf6550d34e8c');
